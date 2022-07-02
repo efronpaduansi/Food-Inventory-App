213203e72a429df292cf49bbe55b74bb27009c64
efronpaduansi <eufrondpaduansi@gmail.com>
2022-07-02 14:14:07 +0700

    update delete orders

--- functions/orders_delete.php
+++ functions/orders_delete.php
@@ -9,19 +9,24 @@
     $fetchIdMenu = $getIdMenu->fetch_assoc();
     $id_menu = $fetchIdMenu['idMenu'];
     
+    $getJml = $conn->query("SELECT jumlah FROM orders WHERE id='$id'");
+    $fetchJml = $getJml->fetch_assoc();
+    $jml = $fetchJml['jumlah'];
+   
 
     $hapus = $conn->query("DELETE FROM orders WHERE id = '$id'");
     if($hapus){
          //menghitung total stock
-         $total = $conn->query("SELECT SUM(jumlah) AS total FROM orders WHERE id_menu = '$id_menu'");
+         $total = $conn->query("SELECT total FROM stock WHERE id_menu = '$id_menu'");
          $row = $total->fetch_assoc();
          $total = $row['total'];
 
+         $sisa = $total - $jml;
+      
          //update table stock
          $updateStok = $conn->query("UPDATE stock SET
-                        id_menu     = '$id_menu',
-                        total       = '$total'
-                        WHERE id_menu = '$id_menu'
+                        total           = '$sisa'
+                        WHERE id_menu   = '$id_menu'
                         ");
          header("location:../view/superadmin/orders.php?hapus=sukses");
     }else{
